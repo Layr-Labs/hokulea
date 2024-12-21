86182b6725af0e007e08442a6577817ebfaa7373
Ubuntu <ubuntu@ip-172-31-39-89.ec2.internal>
2024-12-21 17:41:07 +0000

    move decode logic

--- crates/eigenda/src/eigenda.rs
+++ crates/eigenda/src/eigenda.rs
@@ -2,21 +2,19 @@
 //! [DataAvailabilityProvider] trait for the EigenDA protocol.
 
 use crate::eigenda_blobs::EigenDABlobSource;
-use crate::traits::EigenDABlobProvider;
 use crate::errors::CodecError;
+use crate::traits::EigenDABlobProvider;
 
 use alloc::{boxed::Box, fmt::Debug};
 use alloy_primitives::Bytes;
-use bytes::buf::Buf;
 use async_trait::async_trait;
 use kona_derive::{
+    errors::{PipelineEncodingError, PipelineError, PipelineErrorKind},
     sources::EthereumDataSource,
     traits::{BlobProvider, ChainProvider, DataAvailabilityProvider},
     types::PipelineResult,
-    errors::{PipelineErrorKind, PipelineError, PipelineEncodingError},
 };
 use op_alloy_protocol::BlockInfo;
-use rust_kzg_bn254::helpers::remove_empty_byte_from_padded_bytes_unchecked;
 
 /// A factory for creating an Ethereum data source provider.
 #[derive(Debug, Clone)]
@@ -66,12 +64,7 @@ where
         // just dump all the data out
         info!(target: "eth-datasource", "next item {:?}", item);
 
-        let padded_eigenda_blob = self.eigenda_source.next(&item).await?;
-        info!(target: "eigenda-datasource", "eigenda_source_result {:?}", padded_eigenda_blob);
-
-        // get the actual blob as encoded inside blob
-        let eigenda_blob = self.default_decode_blob(padded_eigenda_blob)?;
-
+        let eigenda_blob = self.eigenda_source.next(&item).await?;
         Ok(eigenda_blob)
     }
 
@@ -79,32 +72,4 @@ where
         self.eigenda_source.clear();
         self.ethereum_source.clear();
     }
-}
-
-impl<C, B, A> EigenDADataSource<C, B, A> 
-where
-    C: ChainProvider + Send + Sync + Clone + Debug,
-    B: BlobProvider + Send + Sync + Clone + Debug,
-    A: EigenDABlobProvider + Send + Sync + Clone + Debug,
-{
-    // https://github.com/Layr-Labs/eigenda/blob/1345e77c8a91fed8e5e33f02c3e32c9ed9921670/api/clients/codecs/default_blob_codec.go#L44
-    fn default_decode_blob(&self, padded_eigenda_blob: Bytes) -> PipelineResult<Bytes> {
-        if padded_eigenda_blob.len() < 32 {
-            // ToDo format error better
-            //return Err(PipelineErrorKind::Temporary(PipelineError::BadEncoding(PipelineEncodingError::SpanBatchError(()))));
-            unimplemented!()
-        }
-
-        info!(target: "eigenda-datasource", "padded_eigenda_blob {:?}", padded_eigenda_blob);
-
-        let content_size = padded_eigenda_blob.slice(2..6).get_u32();
-        info!(target: "eigenda-datasource", "content_size {:?}", content_size);
-        let codec_data = padded_eigenda_blob.slice(32..);
-
-        let blob_content = remove_empty_byte_from_padded_bytes_unchecked(codec_data.as_ref());
-        let blob_content: Bytes = blob_content.into();
-
-        Ok(blob_content.slice(..content_size as usize))
-    }
-
 }
--- crates/proof/src/eigenda_provider.rs
+++ crates/proof/src/eigenda_provider.rs
@@ -2,7 +2,7 @@ use alloc::boxed::Box;
 use alloc::sync::Arc;
 use alloy_primitives::{keccak256, Bytes};
 use async_trait::async_trait;
-use hokulea_eigenda::{EigenDABlobProvider, BlobInfo};
+use hokulea_eigenda::{BlobInfo, EigenDABlobProvider};
 use kona_preimage::{CommsClient, PreimageKey, PreimageKeyType};
 
 use kona_proof::errors::OracleProviderError;
@@ -50,9 +50,8 @@ impl<T: CommsClient + Sync + Send> EigenDABlobProvider for OracleEigenDAProvider
             .await
             .map_err(OracleProviderError::Preimage)?;
 
-
         let mut blob: Vec<u8> = vec![0; cert_blob_info.blob_header.data_length as usize];
-        
+
         // 96 because our g1 commitment has 64 bytes in v1
         let mut field_element_key = [0u8; 96];
 
@@ -60,7 +59,7 @@ impl<T: CommsClient + Sync + Send> EigenDABlobProvider for OracleEigenDAProvider
         let data_length = cert_blob_info.blob_header.data_length as u64 / 32;
 
         info!("cert_blob_info.blob_header.data_length {:?}", data_length);
-        
+
         field_element_key[..32].copy_from_slice(&cert_blob_info.blob_header.commitment.x);
         field_element_key[32..64].copy_from_slice(&cert_blob_info.blob_header.commitment.y);
         for i in 0..data_length {
@@ -69,14 +68,17 @@ impl<T: CommsClient + Sync + Send> EigenDABlobProvider for OracleEigenDAProvider
             let mut field_element = [0u8; 32];
             self.oracle
                 .get_exact(
-                    PreimageKey::new(*keccak256(field_element_key), PreimageKeyType::GlobalGeneric),
+                    PreimageKey::new(
+                        *keccak256(field_element_key),
+                        PreimageKeyType::GlobalGeneric,
+                    ),
                     &mut field_element,
                 )
                 .await
                 .map_err(OracleProviderError::Preimage)?;
             blob[(i as usize) << 5..(i as usize + 1) << 5].copy_from_slice(field_element.as_ref());
         }
-        
+
         info!("cert_blob_info blob {:?}", blob);
 
         Ok(blob.into())
