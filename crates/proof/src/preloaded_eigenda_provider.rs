99c1406c151a364594ddd8ecb146901680c9b2ea
Bowen Xue <bxue@eigenlabs.org>
2025-02-14 12:05:51 -0800

    t1

--- crates/proof/src/preloaded_eigenda_provider.rs
+++ crates/proof/src/preloaded_eigenda_provider.rs
@@ -1,10 +1,17 @@
 use alloy_primitives::{B256, Bytes};
+use kzg_crypto::EigenDABlobWitness;
+use eigenda_v2_struct_rust::v2_cert::rs_struct::EigenDAV2Cert;
+
 
 #[derive(Clone, Debug, Default)]
 pub struct PreloadedEigenDABlobProvider {
-    entries: Vec<(B256, Bytes)>,
+    entries: Vec<(EigenDAV2Cert, Bytes, bool)>,
 }
 
 impl From<EigenDABlobWitness> for PreloadedBlobProvider {
-
+    fn from(value: EigenDABlobWitness) -> Self {
+        return PreloadedBlobProvider {
+            entries: 
+        }
+    }
 }
