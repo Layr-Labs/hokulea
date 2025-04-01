04f4b05640249106132008b3b6c4a4686223ef72
BowenX <bxue@eigenlabs.org>
2025-03-31 23:00:38 -0700

    remove risc0 due to sp1 conflicts

--- crates/compute-proof/src/view_proof.rs
+++ crates/compute-proof/src/view_proof.rs
@@ -1,6 +1,7 @@
 //! This is the crate for generating the view proof for a da cert.
 use crate::steel;
 use eigenda_v2_struct_rust::EigenDAV2Cert;
+/* 
 use risc0_zkvm::Receipt;
 
 /// This function computes view proof. The proof either shows the cert is valid or invalid
@@ -9,3 +10,4 @@ use risc0_zkvm::Receipt;
 pub fn compute_view_proof(cert: &EigenDAV2Cert) -> Receipt {
     steel::compute_view_proof_steel(cert)
 }
+*/
