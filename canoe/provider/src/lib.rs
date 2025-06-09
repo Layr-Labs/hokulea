b8ac1787955d29ebc14739c1355aabc8b84ec513
BowenX <bxue@eigenlabs.org>
2025-06-09 10:50:11 -0700

    lint

--- canoe/provider/src/lib.rs
+++ canoe/provider/src/lib.rs
@@ -59,22 +59,22 @@ pub enum CertVerifierCall {
 
 impl CertVerifierCall {
     /// convert eigenda cert type into its solidity type that works with solidity cert verifier interface
-pub fn build_call(altda_commitment: &AltDACommitment) -> Self {
-    match &altda_commitment.versioned_cert {
-        EigenDAVersionedCert::V2(cert) => {
-            CertVerifierCall::V2(IEigenDACertVerifier::verifyDACertV2ForZKProofCall {
-                batchHeader: cert.batch_header_v2.to_sol(),
-                blobInclusionInfo: cert.blob_inclusion_info.clone().to_sol(),
-                nonSignerStakesAndSignature: cert.nonsigner_stake_and_signature.to_sol(),
-                signedQuorumNumbers: cert.signed_quorum_numbers.clone(),
-            })
-        }
-        EigenDAVersionedCert::V3(cert) => {
-            let v3_soltype_cert = cert.to_sol();
-            CertVerifierCall::Router(IEigenDACertVerifierRouter::checkDACertCall {
-                abiEncodedCert: v3_soltype_cert.abi_encode().into(),
-            })
+    pub fn build_call(altda_commitment: &AltDACommitment) -> Self {
+        match &altda_commitment.versioned_cert {
+            EigenDAVersionedCert::V2(cert) => {
+                CertVerifierCall::V2(IEigenDACertVerifier::verifyDACertV2ForZKProofCall {
+                    batchHeader: cert.batch_header_v2.to_sol(),
+                    blobInclusionInfo: cert.blob_inclusion_info.clone().to_sol(),
+                    nonSignerStakesAndSignature: cert.nonsigner_stake_and_signature.to_sol(),
+                    signedQuorumNumbers: cert.signed_quorum_numbers.clone(),
+                })
+            }
+            EigenDAVersionedCert::V3(cert) => {
+                let v3_soltype_cert = cert.to_sol();
+                CertVerifierCall::Router(IEigenDACertVerifierRouter::checkDACertCall {
+                    abiEncodedCert: v3_soltype_cert.abi_encode().into(),
+                })
+            }
         }
     }
-}
 }
