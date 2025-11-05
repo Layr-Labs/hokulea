410226398f63ddc3b0a56a5dae2969de45121067
BowenX <bxue@eigenlabs.org>
2025-11-04 18:39:45 -0800

    bump sp1cc, update vkey and elf

--- canoe/sp1-cc/host/src/lib.rs
+++ canoe/sp1-cc/host/src/lib.rs
@@ -203,6 +203,7 @@ async fn get_sp1_cc_proof(
                     .await
                     .map_err(|e| anyhow::anyhow!(e.to_string()))?;
 
+                info!("returns_bytes {}", returns_bytes);
                 let returns = <StatusCode as SolType>::abi_decode(&returns_bytes)
                     .expect("deserialize returns_bytes");
                 let is_valid = returns == StatusCode::SUCCESS;
