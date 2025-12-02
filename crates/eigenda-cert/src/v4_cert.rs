92f3897f8e4db418a7eae1f7cf79e163784ddc4c
bxue-l2 <bxue@eigenlabs.org>
2025-12-01 16:50:24 -0800

    add v4 data structure and derivation version

---
+++ crates/eigenda-cert/src/v4_cert.rs
@@ -0,0 +1,39 @@
+use alloy_primitives::Bytes;
+use alloy_primitives::{keccak256, B256};
+use alloy_rlp::{Encodable, RlpDecodable, RlpEncodable};
+use canoe_bindings;
+use serde::{Deserialize, Serialize};
+
+use crate::{BatchHeaderV2, BlobInclusionInfo, NonSignerStakesAndSignature};
+
+extern crate alloc;
+use alloc::vec::Vec;
+
+/// EigenDA CertV3
+#[derive(Debug, Clone, RlpEncodable, RlpDecodable, PartialEq, Serialize, Deserialize)]
+pub struct EigenDACertV4 {
+    pub batch_header_v2: BatchHeaderV2,
+    pub blob_inclusion_info: BlobInclusionInfo,
+    pub nonsigner_stake_and_signature: NonSignerStakesAndSignature,
+    pub signed_quorum_numbers: Bytes,
+    pub offchain_derivation_version: u8,
+}
+
+impl EigenDACertV4 {
+    pub fn to_digest(&self) -> B256 {
+        let mut cert_rlp_bytes = Vec::<u8>::new();
+        self.encode(&mut cert_rlp_bytes);
+        keccak256(&cert_rlp_bytes)
+    }
+
+    pub fn to_sol(&self) -> canoe_bindings::EigenDACertV4 {
+        canoe_bindings::EigenDACertV4 {
+            batchHeaderV2: self.batch_header_v2.to_sol(),
+            blobInclusionInfo: self.blob_inclusion_info.to_sol(),
+            nonSignerStakesAndSignature: self.nonsigner_stake_and_signature.to_sol(),
+            // solidity translate of bytes is alloy-primitives::Bytes
+            signedQuorumNumbers: self.signed_quorum_numbers.clone(),
+            offchainDerivationVersion: self.offchain_derivation_version,
+        }
+    }
+}
