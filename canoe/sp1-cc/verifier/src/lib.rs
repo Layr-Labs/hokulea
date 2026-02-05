57957e9faee4041a136d1e8855eb765b4ec81f33
fakedev9999 <taehoon@succinct.xyz>
2026-02-05 15:20:14 -0800

    chore: bump SP1 from v6.0.0-beta.1 to v6.0.0-rc.1
    Update all SP1 crate versions, CI sp1up/docker tags, justfile
    default, and doc comments to 6.0.0-rc.1.
    
    Note: ELF rebuild and V_KEY update pending (requires rc.1
    toolchain + Docker).

--- canoe/sp1-cc/verifier/src/lib.rs
+++ canoe/sp1-cc/verifier/src/lib.rs
@@ -17,10 +17,10 @@ use sp1_cc_client_executor::ChainConfig;
 use tracing::info;
 
 /// Any change to sp1-cc client including new sp1 toolchain produces a new ELF to be executed and proved by zkVM
-/// To generate the new ELF (a newer version than 6.0.0-beta.1 toolchain tag is also fine)
+/// To generate the new ELF (a newer version than 6.0.0-rc.1 toolchain tag is also fine)
 /// ``` bash
 /// cd canoe/sp1-cc/client
-/// cargo prove build --output-directory ../elf --elf-name canoe-sp1-cc-client --docker --tag v6.0.0-beta.1
+/// cargo prove build --output-directory ../elf --elf-name canoe-sp1-cc-client --docker --tag v6.0.0-rc.1
 /// ```
 ///
 /// The verificaiton of the ELF must be hardcoded here which pins an exact version of ELF a prover can use
